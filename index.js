14bda245d85491c9dc88987c01c788a4158a0e53
BrianNjogu <briannjogu85@gmail.com>
2024-10-03 10:55:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
-console.log (companyname)
 
 let mostProfitableNeighborhood = 'Chelsea';
-console.log (mostProfitableNeighborhood)
 
-let companyCeo = 'SusanSmith';
-console.log (companyCeo)
+let companyCeo = 'Susan Smith';
