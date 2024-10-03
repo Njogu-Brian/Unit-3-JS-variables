aebbe94d86e912cf4b79becbf5cf99486ec60dd9
BrianNjogu <briannjogu85@gmail.com>
2024-10-03 10:47:09 +0300

    added let and cost variables

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+console.log (companyname)
+
+let mostProfitableNeighborhood = 'Chelsea';
+console.log (mostProfitableNeighborhood)
+
+let companyCeo = 'SusanSmith';
+console.log (companyCeo)
